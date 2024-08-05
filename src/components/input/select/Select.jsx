83e10bda1c30735f8b6f8155706e16496854e4bf
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-08-05 16:22:49 -0300

    estilizando versão inicial de componente de select

--- src/components/input/select/Select.jsx
+++ src/components/input/select/Select.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import Select from 'react-select';
 
-function Select({options, styles}) {
+function SelectBox({options}) {
+
+  const Styles = {
+    control: (provided) => ({
+      ...provided,
+      width: "18vw",
+      minHeight: "2px",
+      height: "34px",
+      padding: "0",
+      alignItems: "start",
+      border: "1.8px solid #e6e7e9",
+      borderRadius: "4px",
+      boxShadow: "none",
+      textAlign: "left",
+      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+      transition: "border 0.3s",
+    }),
+    option: (provided) => ({
+      ...provided,
+
+    })
+  }
+
   return (
     <>
-      <Select options = {options} styles = {styles}/>
+      <Select 
+        options = {options} 
+        styles = {Styles}
+      />
     </>
   )
 }
 
-export default Select
+export default SelectBox
--- src/views/gestao-parametros/info/Info.jsx
+++ src/views/gestao-parametros/info/Info.jsx
@@ -2,6 +2,7 @@ import {React, useState, useEffect} from 'react';
 import {Link,useOutletContext} from 'react-router-dom';
 import { Textbox } from '../../../components/input/text-box/Textbox';
 import { Button } from '../../../components/input/botao/Button';
+import SelectBox from '../../../components/input/select/Select';
 import GestaoConteudo from '../gestao-conteudo/GestaoConteudo';
 
 function Info() {
@@ -15,12 +16,22 @@ function Info() {
   const [input1Focused, setInput1Focused] = useState(false);
   const [input1Value, setInput1Value] = useState('');   
 
-   
+  const Options = [
+    {value: "imagem", label: "Imagem"},
+    {value: "imagem", label: "Imagem"},
+    {value: "imagem", label: "Imagem"},
+    {value: "imagem", label: "Imagem"},
+    {value: "imagem", label: "Imagem"},
+  ];
+
   return (
     <>
       <div className="area-superior-info">
         <div className="filtros">
           <div className="input-container">
+            <SelectBox
+              options={Options}
+            />
             <Textbox  
               inputFocused={input1Focused} 
               inputValue={input1Value} 
